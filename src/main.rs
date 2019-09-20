4909a119d16e9774f50754e423e0b5d8d3c2d67d
ahirner <a.hirner@gmail.com>
2019-09-20 21:05:30 +0200

    cv2 capture loop

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use opencv::core;
+use opencv::videoio;
+use opencv::highgui;
+
+fn run(dev: i32, winname: &str) -> opencv::Result<()> {
+
+    let mut cam = videoio::VideoCapture::new_with_backend(dev, videoio::CAP_ANY)?;
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+
+    if !opened
+    {
+        panic!(format!("Unable to open catpure device {}", dev));
+    }
+
+    println!("Opened VideoCapture device: {}", dev);
+
+    highgui::named_window(winname, highgui::WINDOW_AUTOSIZE)?;
+
+    let mut img = core::Mat::default()?;
+    loop {
+
+        let res = cam.read(&mut img)?;
+        if !res | (img.size()?.width <= 0) {
+            println!("Got no image or no good result");
+            break;
+        }
+        highgui::imshow(winname, &img)?;
+
+        let key = highgui::wait_key(1)?;
+
+        if key == 27 { break; }
+    }
+
+    highgui::destroy_window(winname)?;
+
+    Ok(())
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    run(0, "test").unwrap();
+}
