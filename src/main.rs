5148801149bfa126e8498a990b1a027dd1bbe6ae
ahirner <a.hirner@gmail.com>
2019-09-22 19:04:02 +0200

    Iterator camera images but hit streaming/GADT life-time limitation

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,88 @@
+#[feature(async_await)]
 extern crate opencv;
 extern crate tch;
 
 use opencv::core;
-use opencv::videoio;
 use opencv::highgui;
+use opencv::videoio;
 
 use tch::Tensor;
 
-fn run(dev: i32, winname: &str) -> opencv::Result<()> {
+struct CameraCV {
+    cam: videoio::VideoCapture,
+    buf: core::Mat,
+}
 
-    let mut cam = videoio::VideoCapture::new_with_backend(dev, videoio::CAP_ANY)?;
-    let opened = videoio::VideoCapture::is_opened(&cam)?;
-
-    if !opened
-    {
-        panic!(format!("Unable to open catpure device {}", dev));
-    }
-
-    println!("Opened VideoCapture device: {}", dev);
-
-    highgui::named_window(winname, highgui::WINDOW_AUTOSIZE)?;
-
-    let mut img = core::Mat::default()?;
-    loop {
-
-        let res = cam.read(&mut img)?;
-        if !res | (img.size()?.width <= 0) {
-            println!("Got no image or no good result");
-            break;
+impl CameraCV {
+    fn open(device: i32) -> Result<CameraCV, opencv::Error> {
+        let mut cam = videoio::VideoCapture::new_with_backend(device, videoio::CAP_ANY)?;
+        let opened = videoio::VideoCapture::is_opened(&cam)?;
+        if !opened {
+            let msg = format!("Cannot open device {}", device);
+            return Err(opencv::Error::new(0, String::from(msg)));
         }
-        highgui::imshow(winname, &img)?;
+        let mut buf = core::Mat::default()?;
 
-        let key = highgui::wait_key(1)?;
-
-        if key == 27 { break; }
+        Ok(CameraCV { cam, buf })
     }
+}
 
-    highgui::destroy_window(winname)?;
+// Cannot use Iterators for streaming easily without GADTs:
+// https://stackoverflow.com/questions/30422177/how-do-i-write-an-iterator-that-returns
+// -references-to-itself
+// http://lukaskalbertodt.github.io/2018/08/03/solving-the-generalized-streaming-iterator-problem-without-gats.html
 
-    Ok(())
+impl Iterator for CameraCV {
+    type Item = opencv::Result<&core::Mat>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Why is there no ? for Some(Err)
+        let res = self.cam.read(&mut self.buf);
+
+        match res {
+            Err(e) => {
+                return Some(Err(e));
+            }
+            Ok(res_ok) => {
+                if !res_ok {
+                    eprintln!("Camera read bad result: {}", res_ok);
+                    return None;
+                }
+            }
+        }
+
+        let size = self.buf.size();
+        match size {
+            Err(e) => return Some(Err(e)),
+            Ok(size) => {
+                if size.width <= 0 {
+                    eprintln!(
+                        "Camera produced bad image with size: {}x{}",
+                        size.width, size.height
+                    );
+                    return None;
+                }
+            }
+        }
+
+        Some(Ok(&self.buf))
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 
-    run(0, "test").unwrap();
+    let cam = CameraCV::open(0).expect("Cannot open camera");
 
-    let t = Tensor::of_slice(&[10,10]);
+    for img in cam {
+        highgui::imshow("threaded", &img.unwrap()).unwrap();
+
+        let key = highgui::wait_key(1).unwrap();
+        if key == 27 {
+            break;
+        }
+    }
+
+    let t = Tensor::of_slice(&[10, 10]);
     println!("I can print tensors like {:?}", t);
 }
